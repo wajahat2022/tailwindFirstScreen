b8f5fcf4f1596e5d7969ec0f14e7c0872d4cb6eb
HAFIZ WAJAHAT HUSSAIN <104355919+wajahat2022@users.noreply.github.com>
2024-03-15 01:36:43 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const burgetMenu = document.querySelector(".burget-menu");
+const body = document.querySelector("body");
+burgetMenu.addEventListener("click", function () {
+  body.style.backgroundColor = "#A90D1D";
+});
